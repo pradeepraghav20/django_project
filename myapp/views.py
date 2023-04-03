33ab0ffe106a8741258ae2ccee1c90ad1e4c3426
pradeep <pradeepraghav@gmail.com>
2023-04-03 18:26:13 +0530

    comment added

--- myapp/views.py
+++ myapp/views.py
@@ -3,9 +3,8 @@ from django.http import HttpResponse
 
 def index_test(request):
 
-    print("this is indexpage")
+    #print("this is indexpage")
     return HttpResponse ("this is index page")
 
 def index_2(request):
      return HttpResponse ("this is index page 2")
-
